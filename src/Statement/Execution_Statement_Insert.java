596167066d444c27aaac7e128460654804737f7c
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-04-21 15:35:53 +0530

    Inserting data using jdbc added

--- src/Statement/Execution_Statement_Insert.java
+++ src/Statement/Execution_Statement_Insert.java
@@ -1,4 +1,20 @@
 package Statement;
 
+import java.sql.*;
+
 public class Execution_Statement_Insert {
+    public static void main (String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/telusko";
+        String userName = "postgres";
+        String password = "";
+
+        String sql = "INSERT INTO public.product (id, name, type, place, warrenty) VALUES (22, 'Thermal Paste', '2', 'Colombo', 2025)";
+
+        Connection con = DriverManager.getConnection(url, userName,password);
+        Statement st = con.createStatement();
+        int effectedRows = st.executeUpdate(sql);
+
+        System.out.println("Effected Rows : " + effectedRows);
+        con.close();
+    }
 }
